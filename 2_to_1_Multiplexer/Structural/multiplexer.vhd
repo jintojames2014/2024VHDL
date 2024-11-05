fee17eb3ca0dd8c214318ec4d4045c0e996b4885
Jinto Jolly James <jintojollyjames@outlook.com>
2024-11-05 12:06:16 -0600

    multiplexer architecture added
    Added 'structural' architecture for multiplexer

--- 2_to_1_Multiplexer/Structural/multiplexer.vhd
+++ 2_to_1_Multiplexer/Structural/multiplexer.vhd
@@ -9,8 +9,32 @@ ENTITY multiplexer IS PORT (
 END multiplexer;
 
 ARCHITECTURE structural OF multiplexer IS
+
+	COMPONENT notgate IS PORT (
+		i	: IN	STD_LOGIC;
+		o 	: OUT	STD_LOGIC);
+	END COMPONENT;
+	
+	COMPONENT and2gate IS PORT (
+		i1	: IN	STD_LOGIC;
+		i2	: IN	STD_LOGIC;
+		O	: OUT	STD_LOGIC);
+	END COMPONENT;
+	
+	COMPONENT or2gate IS PORT (
+		i1	: IN	STD_LOGIC;
+		i2	: IN 	STD_LOGIC;
+		O	: OUT	STD_LOGIC);
+	END COMPONENT;
+	
+	SIGNAL sn, snd0, sd1	: STD_LOGIC;
+	
 BEGIN
 
--- TO DO: (1)
+	U1	: notgate 	PORT MAP (s, sn);
+	U2	: and2gate	PORT MAP (sn, d0, snd0);
+	U3	: and2gate	PORT MAP (s, d1, sd1);
+	U4	: or2gate 	PORT MAP (snd0, sd1, y);
+	
 
 END Structural;
